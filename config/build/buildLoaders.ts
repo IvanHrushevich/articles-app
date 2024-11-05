c71fcdcf7778282f6cc96a93dd248ebaba9bbe18
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2024-11-06 01:56:15 +0300

    feat: update css setup: add MiniCssExtractPlugin, add css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,4 +1,6 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BuildOptions } from './types/config';
 
 const typescriptLoader = {
   test: /\.tsx?$/,
@@ -6,18 +8,25 @@ const typescriptLoader = {
   exclude: /node_modules/,
 };
 
-const cssLoader = {
-  test: /\.s[ac]ss$/i,
-  use: [
-    // Creates `style` nodes from JS strings
-    'style-loader',
-    // Translates CSS into CommonJS
-    'css-loader',
-    // Compiles Sass to CSS
-    'sass-loader',
-  ],
-};
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      {
+        loader: 'css-loader',
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes('.module.'),
+            localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:5]',
+          },
+        },
+      },
+      // Compiles Sass to CSS
+      'sass-loader',
+    ],
+  };
 
-export function buildLoaders(): webpack.RuleSetRule[] {
   return [typescriptLoader, cssLoader];
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,6 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 import { BuildOptions } from './types/config';
 
@@ -9,5 +10,9 @@ export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInst
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
   ];
 }
