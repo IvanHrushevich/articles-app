f22954ee65da51062361225a2090a128a5f8908a
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2024-11-11 02:36:08 +0300

    refactor: add ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import { Counter } from './components/Counter';
 import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { Suspense } from 'react';
+import { Suspense, useContext } from 'react';
+import { Theme, ThemeContext } from './theme/ThemeContext';
 
 export const App = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  const toggleTheme = () => {
+    setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark);
+  };
+
   return (
-    <div className="app light">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
