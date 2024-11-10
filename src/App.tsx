dc5a1b0a8c4014bd6f5cd92505c79f746d49e032
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2024-11-10 22:59:59 +0300

    feat: add basic styles setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import { Counter } from './components/Counter';
-import './index.scss';
+import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
 
 export const App = () => {
   return (
-    <div className="app">
+    <div className="app light">
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
