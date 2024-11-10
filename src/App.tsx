d821b0997b69c9efe86edec5e340093e739c084a
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2024-11-10 22:22:05 +0300

    feat: add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import { Counter } from './components/Counter';
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { Suspense } from 'react';
 
 export const App = () => {
   return (
     <div className="app">
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'about'} element={<AboutPageAsync />}></Route>
+          <Route path={'/'} element={<MainPageAsync />}></Route>
+        </Routes>
+      </Suspense>
+
       <Counter />
     </div>
   );
